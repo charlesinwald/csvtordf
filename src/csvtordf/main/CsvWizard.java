93b7ce4134409e77eae53c05df6c668072ab0289
Cody D'Ambrosio <cjd218@lehigh.edu>
2020-04-06 09:22:05 -0400

    Add trivial JavaFX functionality
    Add sample usage of modifying Jena model with
    CsvWizard
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- src/csvtordf/main/CsvWizard.java
+++ src/csvtordf/main/CsvWizard.java
@@ -22,17 +22,40 @@ import java.util.concurrent.*;
 // Java GUI
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.event.*;
+
+// Jena imports
+import org.apache.jena.base.Sys;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.shared.*;
 
 public class CsvWizard extends Application {
 
   @Override
   public void start(Stage stage) {
-    Label l = new Label("Hello World!");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
+    Label l = new Label("Prefix:");
+    TextField prefixField = new TextField(CsvToRdf.prefix);
+    prefixField.setPrefColumnCount(30);
+    Button submit = new Button("Submit");
+    HBox hb = new HBox();
+    hb.getChildren().add(l);
+    hb.getChildren().add(prefixField);
+    hb.getChildren().add(submit);
+    Scene scene = new Scene(hb, 640, 480);
+    
     stage.setScene(scene);
     stage.show();
+
+    submit.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent e) {
+        CsvToRdf.prefix = prefixField.getText();
+        stage.close();
+      }
+    });
+
   }
 }
