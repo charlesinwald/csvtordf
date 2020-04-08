4947e1e9a49ac6daaac5933b9b808c642589d987
Cody D'Ambrosio <cjd218@lehigh.edu>
2020-04-08 12:37:06 -0400

    Revert to JDK 8
    Use JDK 8
    Continue working on plugin for Protege
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- buildrun.sh
+++ buildrun.sh
@@ -6,18 +6,23 @@
 # run CsvToRdf application        #
 # in linux environment.           #
 #                                 #
-# NOTE: Java 9 or later required! #
-#                                 #
 ###################################
 package="csvtordf"
-exe="CsvToRdf"
-javafxmods="controls"
+exe="${package}.main.CsvWizard"
 
-srcdir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/src"
+# Need to be in directory
+wdir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+cd $wdir
+srcdir="src"
+libdir="lib"
 
-export CLASSPATH="lib/*:${srcdir}"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  export CLASSPATH="${libdir}/*:${srcdir}"
+else
+  export CLASSPATH="${libdir}/*;${srcdir}"
+fi
+
 echo "CLASSPATH=${CLASSPATH}"
-MODULEPATH="lib"
 
 # clean
 echo "Cleaning ${srcdir}/${package}"
@@ -25,17 +30,14 @@ rm -rf ${srcdir}/${package}/*/*.class
 
 # build
 echo "Building ${package}/*.java"
-modules=""
-for module in ${javafxmods}; do
-    modules="javafx.${module} ${modules}"
-done
-javac --module-path ${MODULEPATH} --add-modules ${modules} ${srcdir}/${package}/main/*.java
+shopt -s extglob
+javac ${srcdir}/${package}/main/!(CsvPlugin).java
 res=$?
 if [ ${res} -ne 0 ]; then
     exit ${res}
 fi
 
-# run
+# test
 echo "Build Complete! Testing..."
 echo "---------------------------------------------"
 javac ${srcdir}/${package}/test/*.java
@@ -45,14 +47,10 @@ if [ ${res} -ne 0 ]; then
     exit ${res}
 fi
 
+# run
 echo "Testing Complete! Running..."
 echo "---------------------------------------------"
-
-#TODO this should eventually be combined into a single graphical app
-#Command line
-#java --module-path ${MODULEPATH} --add-modules ${modules} -Dprism.order=sw ${package}.main.${exe} ${appargs}
-#GUI
-java --module-path ${MODULEPATH} --add-modules ${modules} -Dprism.order=sw ${package}.main.CsvWizard ${appargs}
+java -Dprism.order=sw ${exe}
 res=$?
 
 exit ${res}
---
+++ src/csvtordf/main/CsvPlugin.java
@@ -0,0 +1,42 @@
+/**
+ * CsvPlugin.java
+ * <p>
+ * Authors:
+ * Cody D'Ambrosio || cjd218 || cjd218@lehigh.edu
+ * Charles Inwald  || cci219 || cci219@lehigh.edu
+ * Paul Grocholske || pag314 || pag314@lehigh.edu
+ * <p>
+ * <p>
+ * FIXME: Add description of application
+ */
+package csvtordf.main;
+
+// Java imports
+import java.io.*;
+import java.awt.event.ActionEvent;
+
+// Java GUI
+import javafx.application.Application;
+import javax.swing.JOptionPane;
+
+//  Protege
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.protege.editor.owl.ui.action.ProtegeOWLAction;
+
+public class CsvPlugin extends ProtegeOWLAction {
+
+  public void initialise() throws Exception {}
+
+  public void dispose() throws Exception {}
+
+  public void actionPerformed(ActionEvent event) {
+    String[] args = {};
+    Application.launch(CsvWizard.class, args);
+    StringBuilder message = new StringBuilder("This example menu item is under the Tools menu.\n");
+    message.append("The currently selected class is ");
+    message.append(getOWLWorkspace().getOWLSelectionModel().getLastSelectedClass());
+    message.append(".");
+    JOptionPane.showMessageDialog(getOWLWorkspace(), message.toString());	
+  }
+}
