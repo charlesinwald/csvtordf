0f224d75bf8d624e8d499df7a7c7b3d39c89f006
Cody D'Ambrosio <cjd218@lehigh.edu>
2020-04-15 12:14:43 -0400

    Update buildplugin.sh to check directory
    check we are executing from script home directory
    fix typo in error message for jfxrt.jar copying
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- buildplugin.sh
+++ buildplugin.sh
@@ -8,6 +8,13 @@
 #                                 #
 ###################################
 
+# Need to be in directory
+wdir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+if [ "$(pwd)" != "${wdir}" ]; then
+    echo "ERROR - must buildplugin.sh execute from ${wdir}"
+    exit 2
+fi
+
 # Check for environment variables
 echo "Checking environment variables"
 if [ -z "${PROTEGE_HOME}" ]; then
@@ -27,10 +34,10 @@ elif [[ "${PATH}" != *"ant"* ]]; then
 	exit 2
 fi
 
-# WORKAROUND get version of jfxrt bundled with Protege JRE
+# WORKAROUND - get version of jfxrt bundled with Protege JRE
 rm -f lib/jfxrt.jar && cp "${PROTEGE_HOME}/jre/lib/ext/jfxrt.jar" lib/jfxrt.jar
 if [ $? -ne 0 ]; then
-    echo "ERROR - failed copy jfxrt.jar from ${PROTEGE_HOME}/jre/bin/ext/"
+    echo "ERROR - failed copying jfxrt.jar from ${PROTEGE_HOME}/jre/lib/ext/"
     exit 1
 fi
 
