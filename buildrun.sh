30934b05fced0eb180d97fdf18179fc3eb2c5aed
Cody D'Ambrosio <cjd218@lehigh.edu>
2020-05-04 10:41:55 -0400

    Stop building Jar
    Need to use Ant to build a complete JAR
    
    Signed-off-by: Cody D'Ambrosio <cjd218@lehigh.edu>

--- buildrun.sh
+++ buildrun.sh
@@ -37,28 +37,6 @@ if [ ${res} -ne 0 ]; then
     exit ${res}
 fi
 
-# Create a JAR file too
-echo "Creating Jar"
-# create Manifest file
-mfFile="csvtordf.MF"
-echo "Manifest-Version: 1.0" > ${mfFile}
-echo "Class-Path: src/ " >> ${mfFile}
-for libFile in ${libdir}/*.jar; do
-  if [[ "${libFile}" == *"csvtordf"* || "${libFile}" == *"junit"* || "${libFile}" == *"jfxrt"* ]]; then
-    continue;
-  fi
-  echo " ${libFile} " >> ${mfFile}
-done
-echo " src/" >> ${mfFile}
-echo "Main-Class: ${exe}" >> ${mfFile}
-echo "" >> ${mfFile}
-
-jar cmf ${mfFile} csvtordf.jar ${srcdir}/${package}/main/* lib/*
-res=$?
-if [ ${res} -ne 0 ]; then
-  exit ${res}
-fi
-
 # test
 echo "Build Complete! Testing..."
 echo "---------------------------------------------"
@@ -72,8 +50,7 @@ fi
 # run
 echo "Testing Complete! Running..."
 echo "---------------------------------------------"
-#java -Dprism.order=sw ${exe}
-java -jar csvtordf.jar
+java -Dprism.order=sw ${exe}
 res=$?
 
 exit ${res}
