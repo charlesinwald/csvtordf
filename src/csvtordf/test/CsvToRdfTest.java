282a46b4eaf1fee349888c4ab6a10ffec9cb58a4
Charles Inwald <cci219@lehigh.edu>
2020-05-05 18:37:23 -0400

    readInputFile test

--- src/csvtordf/test/CsvToRdfTest.java
+++ src/csvtordf/test/CsvToRdfTest.java
@@ -11,10 +11,25 @@ public class CsvToRdfTest {
     public void main() {
         System.out.println("Test");
         CsvToRdf program = new CsvToRdf();
+
     }
 
     @Test
     public void readInputFile() {
+        //Single threaded
+        CsvToRdf program = new CsvToRdf();
+        boolean result = program.readInputFile("sample",1);
+        assertTrue(result);
+
+        //Multithreaded
+        int processors = Runtime.getRuntime().availableProcessors();
+        boolean result2 = program.readInputFile("sample", processors);
+        assertTrue(result2);
+
+        //Invalid paths should gracefully fail
+        System.err.println("In the terminal, the line below this should say \"File not found: invalidpath\"");
+        boolean result3 = program.readInputFile("invalidpath", processors);
+        assertFalse(result3);
     }
 
     @Test
